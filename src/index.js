92a8d987ee002365a29e30461b9456622b33b0c7
James Kyburz <james.kyburz@gmail.com>
2017-06-21 17:51:35 +0200

    baseURL typo

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ module.exports = (cookieName) => {
     }
     const uiFlags = { single: true, auth: !!process.env.SERVE_USER }
     const overrideUrl = rootUrls.find((x) => req.url.indexOf(x) !== -1)
-    if (req.url.startsWith(baseUrl)) req.url = req.url.slice(baseUrl.length)
+    if (req.url.startsWith(baseURL)) req.url = req.url.slice(baseURL)
     if (overrideUrl) req.url = overrideUrl
     serve(req, res, uiFlags, uiPath, uiIgnored)
   }
