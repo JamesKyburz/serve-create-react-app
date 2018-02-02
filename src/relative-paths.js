48a6f62236f2c2911cf483768431e2c6cf7e850d
James Kyburz <james.kyburz@gmail.com>
2018-02-02 22:23:39 +0100

    fix relative paths when credentials set

--- src/relative-paths.js
+++ src/relative-paths.js
@@ -2,26 +2,31 @@ const path = require('path')
 const webtouch = require('webtouch')
 const url = require('url')
 
-module.exports = (uiPath) => {
-  const ep = `http://localhost:${process.env.PORT}`
+module.exports = uiPath => {
+  const credentials = process.env.SERVE_USER
+    ? `${process.env.SERVE_USER}:${process.env.SERVE_PASSWORD}@`
+    : ''
+  const ep = `http://${credentials}localhost:${process.env.PORT}`
   return new Promise((resolve, reject) => {
     webtouch(ep, (err, urls) => {
       if (err) return reject(err)
-      resolve([...new Set(
-        urls
-          .map((x) => url.parse(x))
-          .filter((x) => x.host.startsWith('localhost'))
-          .map((x) => x.pathname)
-          .filter((x) => x !== '/')
-          .concat(['/service-worker.js'])
-          .concat(
-            Object.values(
-              require(path.join(uiPath, 'asset-manifest.json'))
-            )
-            .map((x) => '/' + x)
+      resolve(
+        [
+          ...new Set(
+            urls
+              .map(x => url.parse(x))
+              .filter(x => x.host.startsWith('localhost'))
+              .map(x => x.pathname)
+              .filter(x => x !== '/')
+              .concat(['/service-worker.js'])
+              .concat(
+                Object.values(
+                  require(path.join(uiPath, 'asset-manifest.json'))
+                ).map(x => '/' + x)
+              )
           )
-      )]
-      .sort((a, b) => b.length - a.length))
+        ].sort((a, b) => b.length - a.length)
+      )
     })
   })
 }
